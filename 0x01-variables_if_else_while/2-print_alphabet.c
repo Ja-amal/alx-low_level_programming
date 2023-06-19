75252d5116f0024a3b75fa594805be2fc9159933
Ja-amal <amaljafir2@gmail.com>
2023-06-19 05:41:58 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n = 'a';
+while (ch <= 'a')
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
+return (0);
+}
